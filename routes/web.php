b2fb948aaee742837197a59f8fe8a2aee71139d7
Denis <48453257+denisyarif@users.noreply.github.com>
2023-04-27 20:01:33 +0700

    install spatie

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,17 @@ use Inertia\Inertia;
 |
 */
 
+
+
+Route::get('admin', function () {
+    return 'hi admin';
+})->middleware('role:admin');
+
+Route::get('user', function () {
+    return 'hi User';
+})->middleware('role:user');
+
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
